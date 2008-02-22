5dfbbd86b9942a4e4efc27cda5840f02480bb882
Sebastian Sareyko <public@nooms.de>
2008-02-22 21:43:22 +0100

    Made interactive searching in the playlist work

--- album/playlist.py
+++ album/playlist.py
@@ -21,7 +21,7 @@ class PlayList(gtk.TreeView):
         self.set_headers_visible(False)
 
         self.list_store = gtk.ListStore(gobject.TYPE_STRING,
-                gobject.TYPE_STRING, gobject.TYPE_INT)
+                gobject.TYPE_STRING, gobject.TYPE_INT, gobject.TYPE_STRING)
 
         self.pixbuf_renderer = gtk.CellRendererPixbuf()
         self.pixbuf_renderer.set_fixed_size(-1, 32)
@@ -37,7 +37,12 @@ class PlayList(gtk.TreeView):
         self.name_column.add_attribute(self.text_renderer, 'markup', 1)
         self.append_column(self.name_column)
 
+        self.title_column = gtk.TreeViewColumn('name')
+        self.title_column.pack_start(self.text_renderer)
+        self.title_column.add_attribute(self.text_renderer, 'text', 3)
+
         self.set_model(self.list_store)
+        self.set_search_column(3)
 
         self.__xmms.playlist_list(cb=self.__xmms_cb_playlist_list)
         self.__xmms.playlist_list_entries(cb=self.__xmms_cb_entry_list)
@@ -133,7 +138,7 @@ class PlayList(gtk.TreeView):
         self.list_store.append([None,
             '<b>%s</b>\n<small>by</small> %s <small>from</small> %s' %
             (markup_escape_text(title), markup_escape_text(artist),
-                markup_escape_text(album)), id])
+                markup_escape_text(album)), id, title])
 
 
     def remove_entry(self, pos):
