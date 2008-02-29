53d089cfb170fb3f1373ad25b68b9731b6ea4a35
Sebastian Sareyko <public@nooms.de>
2008-02-29 20:54:31 +0100

    Only load the cdrom pixbuf once. Rendering the AlbumList is still pretty slow though :(

--- album/coverart.py
+++ album/coverart.py
@@ -7,19 +7,20 @@ pygtk.require('2.0')
 import gtk
 
 
+CDROM = gtk.Image().render_icon(gtk.STOCK_CDROM, gtk.ICON_SIZE_DND)
+
+
 class CoverArt:
     def __init__(self, data, size):
         pixbuf_loader = gtk.gdk.PixbufLoader()
         self.pixbuf = None
+        global foo
 
         try:
             pixbuf_loader.write(data)
             pixbuf_loader.close()
             self.pixbuf = pixbuf_loader.get_pixbuf()
+            self.pixbuf = self.pixbuf.scale_simple(size, size,
+                    gtk.gdk.INTERP_BILINEAR)
         except Exception, detail:
-            # FIXME: Better use an own icon
-            widget = gtk.Image()
-            self.pixbuf = widget.render_icon(gtk.STOCK_CDROM, gtk.ICON_SIZE_DND)
-
-        self.pixbuf = self.pixbuf.scale_simple(size, size,
-                gtk.gdk.INTERP_BILINEAR)
+            self.pixbuf = CDROM
