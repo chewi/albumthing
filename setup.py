e4bda4225ca305fea33abee5686f7a565cf2dfe2
Sebastian Sareyko <public@nooms.de>
2008-03-04 22:02:16 +0100

    Added a .desktop file

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(name='AlbumThing',
       version='0.1',
       description='A simple XMMS2 client',
       author='Sebastian Sareyko',
+      url='http://nooms.de/',
       packages=['AlbumThing'],
       scripts=['albumthing.py'],
+      data_files=[('share/applications', ['data/AlbumThing.desktop'])],
       )
